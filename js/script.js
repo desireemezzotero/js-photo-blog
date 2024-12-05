79b8f0466a03cadbeca08c92445cef704ec7299f
desireemezzotero <desireemezzotero1@gmail.com>
2024-12-05 17:23:53 +0100

    [add] milestrone3

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
-const endoPoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6';
-const row= document.querySelector('row')
-
-let newArray = [ ]; 
-
-axios.get(endoPoint)
+const endPoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6';
+const row = document.querySelector('.row')
+let ok;
+axios.get(endPoint)
   .then (response => {
     response.data.forEach(card => {
       const {title, url} = card
-      newArray.push(title,url)
-    })
-    console.log(newArray)
-
-
-    row.innerHTML += `
-       <div class="col-12 col-lg-6 col-xl-4 position-relative mb-3">
-
-          <div class="pin position-absolute">
-            <img src="./img/pin.svg" alt="">
-          </div>
-
-          <div class="card-dm p-3">
-            <div class="image debug justify-content-center d-flex">
-              <img src="${url}" alt="">
-            </div> <!-- img -->
-
-            <div class="text text-center debug">
-              <p>
-                ${title}
-              </p>
+      const ok = title.charAt(0).toUpperCase() + title.slice(1)
+      console.log(ok)
+      row.innerHTML += `
+         <div class="col-12 col-lg-6 col-xl-4 position-relative mb-3">
+  
+            <div class="pin position-absolute">
+              <img src="./img/pin.svg" alt="">
             </div>
-          </div><!-- card -->
-        </div><!-- col -->
-    `
+  
+            <div class="card-dm p-4">
+              <div class="image debug">
+                <img src="${url}" alt="" class="img-fluid">
+              </div> <!-- img -->
+  
+              <div class="text text-center debug pt-3">
+                <p>
+                  ${ok}
+                </p>
+              </div>
+            </div><!-- card -->
+          </div><!-- col -->
+      `
+    })
+
   })
+
 
 
 
